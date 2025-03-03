7a92820e12b243a0917a3c833e5c38059926ead8
manibabuthammisetti <manibabu3508@gmail.com>
2025-03-02 19:03:09 -0500

    ci/cd commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,22 @@
 const { defineConfig } = require("cypress");
 
+reportDir: "cypress/reports/mochawesome",
+
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    //setupNodeEvents(on, config) {
       // implement node event listeners here
+      reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/reports/mochawesome",
+      overwrite: false,
+      html: true,
+      json: true,
+      charts: true
     },
   },
 
-  env :{
+  env:{
     URL:'https://naveenautomationlabs.com/opencart/index.php?route=account/register'
   }
 });
