494a9abe799cccbf2742dac42ca2958ee1183245
manibabuthammisetti <manibabu3508@gmail.com>
2025-03-02 19:07:30 -0500

    cypress/e2e/tests/registerTest.cy.js

--- cypress/e2e/tests/registerTest.cy.js
+++ cypress/e2e/tests/registerTest.cy.js
@@ -34,4 +34,18 @@ describe(' test automation', () => {
 
 
     })
+
+
+    it('register flow', () => {
+
+        cy.visit('https://naveenautomationlabs.com/opencart/index.php?route=account/register')
+        cy.get('#input-firstname').type('Mani')
+        cy.get('#input-lastname').type('Babu')
+        cy.get('#input-email').type('Babu@gmail.com')
+        cy.get('#input-telephone').type('12334567890')
+        cy.get('#input-password').type('Manibabu1@')
+        cy.get('#input-confirm').type('Manibabu1@')
+        cy.get('input[type="checkbox').click()
+        cy.get('.btn.btn-primary').click
+    })
 })
